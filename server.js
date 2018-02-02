f7e6cda529dca3c75f1ac7a91e24eb9a4f118d65
Kelvin Wu <wufuhai@gmail.com>
2018-02-02 17:16:31 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || '127.0.0.1';
 // Listen on a specific port via the PORT environment variable
-var port = 80;
+var port = process.env.PORT || 8080;
  
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
